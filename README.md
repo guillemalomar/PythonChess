a2bb286d79ad69ad2bc9a18be74ed47c35c89ad3
Guillem Nicolau Alomar Sitjes <guillemnicolau@gmail.com>
2018-12-24 09:59:07 +0800

    Updated the readme. Updated some outputs. Now the application can be run from terminal (before it was only running from pycharm).

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 *    Title: Python Chess  
 *    Author: Guillem Nicolau Alomar Sitjes      
-*    Current release: December 22nd, 2018                  
-*    Code version: 0.2                       
+*    Current release: December 24nd, 2018                  
+*    Code version: 0.3                     
 *    Availability: Public
 
 ![alt text][logo2]
@@ -16,29 +16,43 @@
     * [Explanation](#explanation)
 * [Using the application](#using-the-application)
     * [Executing](#executing)
+    * [Continuing a previous game](#continuing-a-previous-game)
     * [Testing](#testing)
 
 ## Requirements
 
 - Python +3.5
+- termcolor 1.1.0
+
+I recommend using pip to install the required packages:
+
+_pip install -r requirements.txt_
 
 ## Documentation
 
 ### Explanation
 
-This project is a simple chess game, in Python
+This project is a simple chess game, in Python.
+If you have never heard of Chess, or don't know how to play, check this link:
+
+[Chess(Wikipedia)](https://en.wikipedia.org/wiki/Chess)
 
 ## Using the application
 
 ### Executing
 
+Execute the following command to start the application
 ```
-~/Python-Chess python src/chess.py
+~/Python-Chess python chess.py
 ```
 
-### Testing
+#### Application input parameters
 
-Some tests have been added to the 'tests' folder. To run them, simply type from the main project folder:
+Execute the following command to see the application input parameters.
 ```
-nosetests src/tests
+~/Python-Chess python chess.py --help
 ```
+
+### Continuing a previous game
+
+One of the input parameters is _-c_. If this flag is added, the application will try to continue a game stored in _src/logs/movements.log_. In order to save a game, type 'save' any time an input is required.
--- chess.py
+++ chess.py
@@ -6,26 +6,28 @@ from src.conf.settings import size_x, size_y
 from src.conf.board_init import board_init
 from src.conf.logger import setup_logger
 from src.input_output.inputs import choose_move, choose_piece
-from src.input_output.outputs import print_table, clean_screen, print_legend
+from src.input_output.outputs import print_table, clean_screen, start_print
 from src.moves.moves import move_piece
 
 
 turn_iter = itertools.cycle('WB')
 
 formatter = logging.Formatter('%(message)s')
-current_logger = setup_logger('log1', "logs/current.log", with_formatter=formatter)
-super_logger = setup_logger('log2', "logs/all.log")
+current_logger = setup_logger('log1', "src/logs/current.log", with_formatter=formatter)
+super_logger = setup_logger('log2', "src/logs/all.log")
 
 
 def main_function(coord_x_or, coord_y_or, turn, cont_file=''):
     if cont_file != '':
-        f = open(cont_file, 'r')
+        file = open(cont_file, 'r')
+    else:
+        file = ''
     board = board_init(coord_x_or, coord_y_or)
     print_table(board)
     while True:
         if cont_file != '':
             try:
-                line = next(f)
+                line = next(file)
                 turn_move = line.replace('\n', '')
             except StopIteration:
                 turn_move = False
@@ -34,6 +36,7 @@ def main_function(coord_x_or, coord_y_or, turn, cont_file=''):
             turn_move = False
         position = 0
         correct_move = 0
+        target = 0
         while not correct_move:
             while not position:
                 if turn_move:
@@ -55,14 +58,10 @@ def main_function(coord_x_or, coord_y_or, turn, cont_file=''):
 
 
 if __name__ == "__main__":
-    clean_screen()
-
-    print("##############\n# CHESS GAME #\n##############")
-    print_legend()
 
     # Arguments are taken from command line
 
-    parser = argparse.ArgumentParser(description='Reddit Crawler Client',
+    parser = argparse.ArgumentParser(description='Python Chess',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--sizex', action="store", dest="size_x",
                         help="Horizontal size of the chess board",
@@ -70,16 +69,18 @@ if __name__ == "__main__":
     parser.add_argument('--sizey', action="store", dest="size_y",
                         help="Vertical size of the chess board",
                         default=size_y, type=int)
-    parser.add_argument('--continue', action="store", dest="cont",
-                        help="Name of the log file to continue a name",
-                        default=False, type=str)
+    parser.add_argument('-c', action="store_true", help="If you want to continue a game stored in src/logs/movements.log")
     args = parser.parse_args()
     size_x = args.size_x
     size_y = args.size_y
 
     initial_turn = next(turn_iter)
 
-    if args.cont:
+    clean_screen()
+
+    start_print()
+
+    if args.c:
         main_function(size_x, size_y, turn=initial_turn, cont_file='movements.log')
     else:
         main_function(size_x, size_y, turn=initial_turn)  # TESTING
